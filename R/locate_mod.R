233601598224cdaa5997c4c19f820eddff5a916d
Tsung-Heng Tsai <tsai.tsungheng@gmail.com>
2018-04-16 23:47:03 -0400

    Use peptide starting AA index as input

--- R/locate_mod.R
+++ R/locate_mod.R
@@ -4,17 +4,16 @@
 #' \code{locate_mod} locates modified sites with a peptide.
 #'
 #' @param peptide A string. Peptide sequence.
-#' @param pep_idx A matrix. Starting and ending indices of the peptide.
+#' @param aa_start An integer. Starting index of the peptide.
 #' @param residue_symbol A string. Modification residue and denoted symbol.
 #' @return A string.
 #'
 #' @examples
-#' locate_mod(peptide, pep_idx, residue_symbol)
+#' locate_mod(peptide, aa_start, residue_symbol)
 #'
 #' @export
 #'
-locate_mod <- function(peptide, pep_idx, residue_symbol) {
-    aa_start <- unname(pep_idx[, "start"])
+locate_mod <- function(peptide, aa_start, residue_symbol) {
     mod_rels <- stringr::str_locate_all(peptide, residue_symbol)[[1]]
     mod_rel_idx <- unname(mod_rels[, "start"])
     mod_idx <- mod_rel_idx - seq_along(mod_rel_idx) + aa_start
--- R/locate_site.R
+++ R/locate_site.R
@@ -4,19 +4,17 @@
 #' \code{locate_site} locates modifiable sites with a peptide.
 #'
 #' @param peptide A string. Peptide sequence.
-#' @param pep_idx A matrix. Starting and ending indices of the peptide.
+#' @param aa_start An integer. Starting index of the peptide.
 #' @param residue A string. Amino acid residue for the modification.
 #' @return A string.
 #'
 #' @examples
-#' idx <- matrix(c(2, 5), nrow = 1, dimnames = list(NULL, c("start", "end")))
-#' locate_site("ABCA", idx, "A")
-#' locate_site("ABCA", idx, "A|C")
+#' locate_site("ABCA", 2, "A")
+#' locate_site("ABCA", 2, "A|C")
 #'
 #' @export
 #'
-locate_site <- function(peptide, pep_idx, residue) {
-    aa_start <- unname(pep_idx[, "start"])
+locate_site <- function(peptide, aa_start, residue) {
     aa_rels <- stringr::str_locate_all(peptide, residue)[[1]]
     aa_rel_idx <- unname(aa_rels[, "start"])
     aa_idx <- aa_rel_idx + aa_start - 1
