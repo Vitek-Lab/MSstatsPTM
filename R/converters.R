7dfef033cc678136be8691f29edc04258d775154
tonywu1999 <wu.anthon@northeastern.edu>
2025-07-10 15:05:41 -0700

    fix(maxQuant): Remove unmodified peptides from PTM dataset

--- R/converters.R
+++ R/converters.R
@@ -621,8 +621,6 @@ MaxQtoMSstatsPTMFormat = function(evidence=NULL,
                                                            keep = 1)]
   }
   
-  MSstatsPTMformat = list('PTM' = msstatsptm_input)
-  
   if (!is.null(evidence_prot)){
     annotation_protein = as.data.table(annotation_protein)
     
@@ -643,9 +641,10 @@ MaxQtoMSstatsPTMFormat = function(evidence=NULL,
                                          proteinID = which_proteinid_protein)
     }
     
+    msstatsptm_input = msstatsptm_input[grepl(mod_id, msstatsptm_input$PeptideSequence),]
     MSstatsPTMformat = list('PTM' = msstatsptm_input, 
                             "PROTEIN" = msstats.abun)
-    
+    return(MSstatsPTMformat)
   }
   
   if (use_unmod_peptides){
@@ -654,6 +653,9 @@ MaxQtoMSstatsPTMFormat = function(evidence=NULL,
     
     MSstatsPTMformat = list(PTM = msstatsptm_input, 
                             PROTEIN = msstats.abun)
+  } else {
+    msstatsptm_input = msstatsptm_input[grepl(mod_id, msstatsptm_input$PeptideSequence),]
+    MSstatsPTMformat = list('PTM' = msstatsptm_input)
   }
   
   return(MSstatsPTMformat)
